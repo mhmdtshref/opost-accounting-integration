9b61f10c26c7710a9048054fb869a51fe5b29ac3
Mohamed Sharif <mshareef@itgsoftware.com>
2025-04-07 03:17:54 +0300

    fix naming and icons

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -16,7 +16,7 @@
  * Another way is to clear the cookie from the browser's Application/Storage tab and then reload the page.
  */
 const themeConfig = {
-  templateName: 'Materialize',
+  templateName: 'OPost Integration',
   homePageUrl: '/home',
   settingsCookieName: 'materialize-mui-next-demo-1',
   mode: 'system', // 'system', 'light', 'dark'
